3a00bc78e2d0edb7efdf9fcd0716235ec0573b5b
yohanessahrul <yohanessahrul92@gmail.com>
2024-11-23 05:43:08 +0700

    copy template

--- src/pages/task4/index.tsx
+++ src/pages/task4/index.tsx
@@ -1,7 +1,24 @@
+import HeadHtml from "@/components/head";
+import Layout from "@/components/layouts";
+import Nav from "@/components/nav";
+
 const Task4 = () => {
   return (
     <>
-      <h1>Task 4</h1>
+      <HeadHtml
+        title="[GET] - Users"
+        description="Akses API gorest dan tampilkan daftar users"
+      />
+      <Layout formalHeader={false}>
+        <main className="w-full min-h-screen bg-neutral-200">
+          <div className="lg:w-[990px] md:w-[500px] sm:w-full mx-auto">
+            <Nav task="4" />
+            <div className="bg-neutral-100 shadow-md p-6">
+              <h1 className="text-3xl">[GET] - Users</h1>
+            </div>
+          </div>
+        </main>
+      </Layout>
     </>
   );
 };
