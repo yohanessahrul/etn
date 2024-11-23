53ae896fb921219340644e92e33e42503a37739a
yohanessahrul <yohanessahrul92@gmail.com>
2024-11-23 18:12:02 +0700

    add drawer

--- src/pages/task3/index.tsx
+++ src/pages/task3/index.tsx
@@ -2,36 +2,65 @@ import HeadHtml from "@/components/head";
 import Layout from "@/components/layouts";
 import Nav from "@/components/nav";
 import jsonData from "../../../json/mockaroo.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClose,
+  faEllipsisVertical,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Task3 = () => {
   const [data, setData] = useState([...jsonData]);
   const [name, setName] = useState("");
   const [car, setCar] = useState("");
   const [city, setCity] = useState("");
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    if (openDrawer) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [openDrawer]);
 
   const handleSearch = () => {
     const filterName = [...jsonData].filter((item) => {
-      if (item.full_name.toLowerCase().includes(name)) {
+      if (item.full_name.toLowerCase().includes(name.toLowerCase())) {
         return item;
       }
     });
 
     const filterCar = [...filterName].filter((item) => {
-      if (item.car.toLowerCase().includes(car)) {
+      if (item.car.toLowerCase().includes(car.toLowerCase())) {
         return item;
       }
     });
 
     const filterCity = [...filterCar].filter((item) => {
-      if (item.city.toLowerCase().includes(city)) {
+      if (item.city.toLowerCase().includes(city.toLowerCase())) {
         return item;
       }
     });
 
     setData(filterCity);
   };
+
+  const openDrawerHandler = (customer: any) => {
+    console.log("customer", customer);
+    setOpenDrawer(true);
+    setCustomer(customer);
+  };
+
+  const closeDrawerHandler = () => {
+    setOpenDrawer(true);
+    setCustomer(null);
+  };
+
+  const changeCustomerDataHandler = () => {};
 
   return (
     <>
@@ -40,7 +69,7 @@ const Task3 = () => {
         description="CRUD data sample from mockaroo"
       />
       <Layout formalHeader={false}>
-        <main className="w-full min-h-screen bg-neutral-200">
+        <main className="w-full min-h-screen bg-neutral-200 relative">
           <div className="lg:w-[990px] md:w-[500px] sm:w-full mx-auto ">
             <Nav task="3" />
             <div className="bg-neutral-100 shadow-md p-6">
@@ -105,6 +134,9 @@ const Task3 = () => {
                       <th className="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">
                         City
                       </th>
+                      <th className="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">
+                        <FontAwesomeIcon className="w-6" icon={faGear} />
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -133,6 +165,14 @@ const Task3 = () => {
                         <td className="px-6 py-3 text-start text-md font-medium text-gray-500">
                           {item.city}
                         </td>
+                        <td className="px-6 py-3 text-start text-md font-medium text-gray-500 relative flex justify-center items-center">
+                          <FontAwesomeIcon
+                            stroke="1px"
+                            icon={faEllipsisVertical}
+                            className="hover:cursor-pointer h-6 text-center text-neutral-700 w-8 mt-2"
+                            onClick={() => openDrawerHandler(item)}
+                          />
+                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -140,6 +180,36 @@ const Task3 = () => {
               </div>
             </div>
           </div>
+
+          {/* Drawer */}
+          <div
+            className={`fixed inset-y-0 right-0 bg-white shadow-lg transform z-10 ${
+              openDrawer ? "translate-x-0" : "translate-x-full"
+            } transition-transform duration-300`}
+          >
+            <div className="md:w-[600px] sm:w-full min-h-full p-4 bg-neutral-100">
+              <button
+                className="px-4 py-3 bg-neutral-500 text-neutral-100 rounded-full"
+                onClick={() => setOpenDrawer(!openDrawer)}
+              >
+                <FontAwesomeIcon
+                  className="text-neutral-100 w-4"
+                  icon={faClose}
+                />
+              </button>
+              <div className="border-b-2 py-4">
+                <p>{JSON.stringify(customer)}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Overlay */}
+          {openDrawer && (
+            <div
+              className="fixed inset-0 bg-black bg-opacity-50 bg-neutral-900 z-0"
+              onClick={() => setOpenDrawer(false)}
+            ></div>
+          )}
         </main>
       </Layout>
     </>
